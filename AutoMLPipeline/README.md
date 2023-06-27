efef016d4321e41afd448a28c309c2c48112acda
Ibrainscn <38371097+Ibrainscn@users.noreply.github.com>
2023-06-27 01:40:05 -0700

    Add main code

---
+++ AutoMLPipeline/README.md
@@ -0,0 +1,4 @@
+Step0: Activate your desired virtual environment
+Step1: pip install -r requirements.txt
+Step2: python run_experiment_demo.py
+Step3: tensorboard --logdir=exp_output_demo/logs --port 8001 
---
+++ AutoMLPipeline/run_experiment_lstm.py
@@ -0,0 +1,96 @@
+import os
+import json
+import shutil
+import numpy as np
+import pandas as pd
+from tensorboard import program
+from utils.custom_dataset import data_preprocess
+from utils.nn_lstm import LSTM
+from utils.nn_tcn import TCN
+from utils.runner import Runner
+
+
+def setup_config(config_file):
+    with open(config_file) as f:
+        params = json.load(f)
+
+    return params
+
+
+def setup_dataset(data, params):
+
+    return data_preprocess(data, params)
+
+
+def setup_model(params, data_shape):
+    model_name = params["mlflow_model"]["model_name"]
+    model_params = params["mlflow_model"]["model_params"]
+    win_size = params["mlflow_data"]["win_size"]
+    num_features = data_shape["num_features"]
+    num_classes = data_shape["num_classes"]
+    # input shape: batch_size, win_size, num_features
+    input_shape = (1, win_size, num_features)
+    if model_name == "LSTM":
+        model = LSTM(input_shape, model_params, num_classes)
+    elif model_name == "TCN":
+        model = TCN(input_shape, model_params, num_classes)
+    else:
+        raise ValueError("Model {model_name} has not implemented yet!")
+    
+    return model
+
+
+def setup_expriment(params, data, model):
+
+    return Runner(params, data, model)
+
+
+def launch_tensorboard(logdir):
+    tb = program.TensorBoard()
+    tb.configure(argv=[None, '--logdir', logdir, '--port', '6006'])
+    url = tb.launch()
+    print(f"TensorBoard started at {url}")
+
+
+if __name__ == "__main__":
+    print("Start running the experiment...")
+
+    # Setup config
+    # config_file = "config/example_config.json"
+    config_file = "config/lstm_config.json"
+
+    params = setup_config(config_file)
+
+    # Setup dataset
+    # Example 2D array
+    # arr = np.random.randint(0, 2, size=(1000, 4))
+    with open(os.path.join("data", "Q1_Q2_data_cleaned.csv"), 'r') as f:
+        df = pd.read_csv(f, index_col=0)
+    arr_Xy = df.drop(columns=["block"]).to_numpy()
+    data = setup_dataset(arr_Xy, params)
+
+    # Setup model
+    model = setup_model(params, data["data_shape"])
+
+    # Setup experiment
+    exp = setup_expriment(params, data, model)
+
+    # Remove any previous old logs
+    log_dir = os.path.join(params["mlflow_io"]["output_dir"], "logs")
+    shutil.rmtree(log_dir)
+
+    # Run experiment
+    # Train and validate the model
+    exp.train()
+
+    # Predict
+    exp.predict()
+
+    # Launch TensorBoard to visualize the train/val/test loss and metric
+    launch_tensorboard(log_dir)
+    # Or run the following command in your terminal, then follow the link
+    # tensorboard --logdir=exp_output_lstm/logs --port 8000
+    # tensorboard --logdir=exp_output_tcn/logs --port 8001
+
+
+
